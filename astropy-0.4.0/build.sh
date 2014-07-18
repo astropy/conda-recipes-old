5be38aacea19268ccbf3bed92ec187359a3633c3
Matt Craig <mattwcraig@gmail.com>
2014-07-17 20:33:10 -0500

    Add recipe for astropy-0.4.0

---
+++ astropy-0.4.0/build.sh
@@ -0,0 +1,10 @@
+# use the offline option to prevent querying PyPI for a newer
+# version of astropy-helpers. conda build prevents accessing PyPI
+# so omitting --offline raises an exception when astropy_helpers
+# looks for a new version.
+
+#
+# could also be fixed by adding a configuration item to the
+# ah_bootstrap section, I suppose...
+$PYTHON setup.py install --offline
+
